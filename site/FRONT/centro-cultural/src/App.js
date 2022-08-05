0d54eceacc9b17963a1e133dde99799b14fbc3e7
Matias Alonso <matiasalosno88@gmail.com>
2022-08-05 12:58:58 -0300

    creacion del navbar y footer

--- site/FRONT/centro-cultural/src/App.js
+++ site/FRONT/centro-cultural/src/App.js
@@ -1,9 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import NavBar from "./components/navBar/NavBar";
+import Home from "./pages/Home";
+import Institucional from "./pages/Institucional";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+
+        <NavBar/>
+      
+        <Routes>
+          <Route path="/" element ={<Home/>} />
+          <Route path="/institucional" element ={<Institucional/>} />
+
+        </Routes>
+
+        <Footer/>
+      
+      </BrowserRouter>
+
 
     </div>
   );
---
+++ site/FRONT/centro-cultural/src/pages/Institucional.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Institucional = () => {
+  return (
+    <div>Institucional</div>
+  )
+}
+
+export default Institucional
