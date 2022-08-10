ec8a5fce8dd68eac2c6728c6bd3b5650abf660b2
tatooliveto <101537485+tatooliveto@users.noreply.github.com>
2022-08-10 12:23:32 -0300

    homeCursosYtalleres

--- site/FRONT/centro-cultural/src/pages/Home.js
+++ site/FRONT/centro-cultural/src/pages/Home.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import '../styles/homecursosTalleres.css'
+
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section id="Cursostalleres">
+    <h2 className="title">Cursos y Talleres</h2><br />
+    <p>El Cultural San Martín ofrece talleres y seminarios presenciales durante los meses de mayo, junio, julio y agosto. Hay cursos de 12 clases de 6 encuentros.<br />
+    <br />
+
+    Todos se llevan a cabo en alguna de las salas del CSM, ubicado en Sarmiento 1551 - CABA. 
+    <br />
+    <br />
+
+    ¡Te esperamos!</p>
+    <br />
+    
+    
+    
+
+    <h2 className="Subtitle artes">Artes Escenicas</h2>
+  
+    </section>
   )
 }
 
