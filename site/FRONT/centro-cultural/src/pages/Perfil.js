20a3e800f1e2c9a82856f0ef2e23c0fd7c9506ac
tatooliveto <101537485+tatooliveto@users.noreply.github.com>
2022-08-24 14:56:24 -0300

    vista perfil DESKTOP

--- site/FRONT/centro-cultural/src/pages/Perfil.js
+++ site/FRONT/centro-cultural/src/pages/Perfil.js
@@ -2,12 +2,24 @@ import React from "react";
 import "../styles/perfil.css";
 import Loginform from "../components/LoginForm/Loginform.jsx";
 
+
 const Perfil = () => {
   return (
     <section id="perfil">
+      
+
       <p className="perfil">Perfil</p>
-      <h1> Ingresar </h1> <br />
+
+
+
+      <div className="LoginBox">
+      
       <Loginform />
+      </div>
+
+
+
+    
       <br />
     </section>
   );
